0873881a896d4b05ed6582208282faa3787a29a2
R-MR-Adao <rm.ribeiro.adao@gmail.com>
2023-04-23 16:18:22 +0200

    [WAR] Small cleanup

--- puzzles/war.py
+++ puzzles/war.py
@@ -6,34 +6,31 @@ def draw_cards(n):
         del cards[player][:n]                           # remove them from deck
 
 def battle():
-    draw_cards(1)                                       # draw card players onto the table
+    draw_cards(1)                                       # draw cards onto the table
     v = value[table[0][-1]]-value[table[1][-1]]         # value difference
     return 1 if v > 0 else 2 if v < 0 else -1           # determine winner
 
 def move_cards(winner):
-    global table                                        # get table from global scope
+    global table                                        # access table from global scope
     cards[winner-1] += table[0] + table[1]              # move cards to winner's deck
     table = [[],[]]                                     # empty table
-    return len(cards[0]) == 0 or len(cards[1]) == 0     # stops game if players have no more cars
+    return len(cards[0]) == 0 or len(cards[1]) == 0     # stops game if players have no more cards
 
 def war():
-    stop = False
     if len(cards[0]) < 4 or len(cards[1]) < 4:          # not enough cards to play war
         global game_winner                              # define global game winner
         game_winner = "PAT"                             # the players draw
-        stop = True                                     # the game ends
+        return True                                     # the game ends
     else:
         draw_cards(3)                                   # draw 3 cards from each player
-        stop = play_round()                             # play another round
-    return stop
+        return play_round()                             # play another round
 
 def play_round():
     winner = battle()                                   # battle phase
     if winner > 0 :                                     # one of the player wins
-        stop = move_cards(winner)
+        return move_cards(winner)                       # move cards from table to winner's deck
     else:                                               # draw
-        stop = war()                                    # start war
-    return stop
+        return war()                                    # start war
 
 def find_winner():
     if game_winner != "":                               # the winner is already known (PAT)
@@ -59,8 +56,8 @@ stop = False                                            # trigger to end game
 
 # game start
 
-while stop == False:                                    # while game is not finished    
+while not stop:                                         # while game is not finished    
     stop = play_round()                                 # play next round
     n_rounds += 1                                       # increment number of rounds
 
-print(find_winner())
+print(find_winner())                                    # prints game result
